52dc155f9c2b291061d6b9eed8b03138bd02340b
Andres Francisco Franco <125943749+Andresfrla@users.noreply.github.com>
2025-04-15 21:52:37 -0600

    Style the grid of the buttons

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, View } from 'react-native';
 import CategoryScreen from './screens/CategoryScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <>
+      <StatusBar styles='light'/>
       <CategoryScreen />
-    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
