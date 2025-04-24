5ea2e0f19292a9645ed369606998f21f626bff0b
Andres Francisco Franco <125943749+Andresfrla@users.noreply.github.com>
2025-04-23 18:51:20 -0600

    Pass the styling of the button by props

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -4,6 +4,7 @@ import MealDetails from "../components/MealDetails";
 import Subtitle from "../components/MealDetail/Subtitle";
 import List from "../components/MealDetail/List";
 import { useLayoutEffect } from "react";
+import IconButton from "../components/IconButton";
 
 function MealDetailScreen({ route, navigation }) {
   const { mealId } = route.params;
@@ -17,7 +18,7 @@ function MealDetailScreen({ route, navigation }) {
   useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => {
-        return <Button title="Tap me!" onPress={headerButtonPressHandler} />;
+        return <IconButton icon="star" color="white" onPress={headerButtonPressHandler} />;
       }
     });
   }, [navigation, headerButtonPressHandler]);
